82c3e0459396fcc1724fabbb41ed1d350473289b
Johanna Merikanto <johanna.merikanto2@pointcollege.fi>
2018-07-03 16:55:08 +0300

    Konsoli -ohjelmien harjoituspohja.

---
+++ DatasetAssignment1/DatasetAssignment1/Program1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatasetAssignment1
+{
+    public class Program1
+    {
+        static void Main(string[] args)
+        {
+            //my first program in c#
+            Console.WriteLine("Helo World");
+          
+        }
+    }
+}
